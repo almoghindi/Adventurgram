14add88236df19903de99ecf880c52a76c2c0667
yonraz <yraz530@gmail.com>
2024-07-17 15:59:49 +0300

    profile srv connected to infra

--- user-profile/user-profile/Startup.cs
+++ user-profile/user-profile/Startup.cs
@@ -1,10 +1,14 @@
 ﻿
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.OpenApi.Models;
 using user_profile.DAL.Data;
 using user_profile.DAL.Repositories;
 using user_profile.Middlewares;
 using user_profile.Services;
+using Npgsql;
+using System.Net;
 
 namespace user_profile
 {
@@ -17,23 +21,45 @@ namespace user_profile
         }
         public void ConfigureServices(IServiceCollection services)
         {
-
+            var connectionString = new NpgsqlConnectionStringBuilder
+            {
+                Host = "postgres-srv",
+                Port = 5432,
+                Database = "user_profiles_db",
+                Username = "ag-admin",
+                Password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD")
+            }.ToString();
 
             services.AddControllers();
             services.AddDbContext<UserProfileContext>(options =>
-                options.UseNpgsql(Configuration["ConnectionStrings:UserProfileContext"]));
+                options.UseNpgsql(connectionString));
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
             services.AddScoped<IUserProfileRepository, UserProfileRepository>();
             services.AddScoped<IUserProfileService, UserProfileService>();
+           
+            // services.Configure<KestrelServerOptions>(options =>
+            // {
+            //     options.Listen(IPAddress.Any, 8081, listenOptions =>
+            //     {
+            //         listenOptions.UseHttps();
+            //     });
+            //     options.Listen(IPAddress.Any, 8080);
+            // });
+
+
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders =
+                    ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            });
 
             services.AddCors(options =>
             {
                 options.AddPolicy("CorsPolicy", builder => builder
-                    .SetIsOriginAllowed(origin => true)
+                    .AllowAnyOrigin()
                         .AllowAnyMethod()
-                        .AllowAnyHeader()
-                        .AllowCredentials());
+                        .AllowAnyHeader());
             });
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -46,7 +72,12 @@ namespace user_profile
 
             app.UseHttpsRedirection();
             app.UseRouting();
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
             app.UseAuthorization();
+            app.UseCors("CorsPolicy");
             app.UseMiddleware<ExceptionHandler>();
 
             app.UseEndpoints(endpoints =>
